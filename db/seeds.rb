690fea6e493d1ff9f5917c90a994ebb07262838c
VartanVA <info@vartan.com.ua>
2016-12-01 18:21:05 +0200

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+Product.delete_all
+
+# . . .
+Product.create!(title: 'Programming Ruby 1',
+description: %{<p> Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox. </p>},
+image_url: '1.jpg',
+price: 49.95)
+
+# . . .
+Product.create!(title: 'Programming Ruby 2',
+description: %{<p> Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox. </p>},
+image_url: '2.jpg',
+price: 49.95)
+
+# . . .
+Product.create!(title: 'Programming Ruby 3',
+description: %{<p> Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox. </p>},
+image_url: '3.jpg',
+price: 49.95)
+
+# . . .
+Product.create!(title: 'Programming Ruby 4',
+description: %{<p> Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox. </p>},
+image_url: '4.jpg',
+price: 49.95)
